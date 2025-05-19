f2fcaed171726dce348969176accc099cd8a3483
Diablo-xit <narutowahab87@gmail.com>
2025-05-19 12:43:24 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,18 @@
+AOS.init();
+
+document.getElementById("animes").addEventListener("click", () => {
+  window.location.href = "https://diablo-xit.github.io/ANIMA.fr";
+});
+
+document.getElementById("audio").addEventListener("click", () => {
+  window.location.href = "https://diablo-xit.github.io/My-weather-/";
+});
+
+document.getElementById("ip").addEventListener("click", () => {
+  window.location.href = "https://diablo-xit.github.io/IP-info/";
+});
+
+setInterval(() => {
+  const date = new Date();
+  document.getElementById("date").innerHTML = `${date.toLocaleTimeString()}`;
+}, 1000);
