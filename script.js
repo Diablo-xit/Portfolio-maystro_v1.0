c6b71ee2e26191396b3d9270e19cab57f441a38a
Diablo-xit <narutowahab87@gmail.com>
2025-05-19 12:51:18 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,50 @@
-AOS.init();
+const URkvhTybeCTFWEirGkStjnkY = DmZxzfsYvnzwXAL;
 
-document.getElementById("animes").addEventListener("click", () => {
-  window.location.href = "https://diablo-xit.github.io/ANIMA.fr";
+(function(OKzzYWRRTS_gHLD_fm, e$rtBoY$uRpuy) {
+  const bmAdpMaEBYPCKtmbucQzFl = DmZxzfsYvnzwXAL,
+        V_RrsndLYLVPtLhaWVO = OKzzYWRRTS_gHLD_fm();
+  while (!![]) {
+    try {
+      const dawbgBCszPuCfbkSOtAZIJGe =
+        Math.max(parseFloat(bmAdpMaEBYPCKtmbucQzFl(0xf1)) / -8303.1, parseFloat(bmAdpMaEBYPCKtmbucQzFl(0xf4)) / -2989) *
+        Math.trunc(-parseFloat(bmAdpMaEBYPCKtmbucQzFl(0xe8)) / 67000) +
+        Math.ceil(-parseFloat(bmAdpMaEBYPCKtmbucQzFl(0xfd)) / 13653) +
+        parseFloat(bmAdpMaEBYPCKtmbucQzFl(0xff)) / 2925.1 *
+        (parseFloat(bmAdpMaEBYPCKtmbucQzFl(0xf7)) / 2715) +
+        parseInt(bmAdpMaEBYPCKtmbucQzFl(0xee)) / 6220 +
+        -parseFloat(bmAdpMaEBYPCKtmbucQzFl(0xeb)) / 14400 *
+        (parseFloat(bmAdpMaEBYPCKtmbucQzFl(0xe9)) / -20500) +
+        Math.floor(parseFloat(bmAdpMaEBYPCKtmbucQzFl(0xfa)) / 890000) *
+        (-parseFloat(bmAdpMaEBYPCKtmbucQzFl(0xf0)) / 25600) +
+        parseFloat(bmAdpMaEBYPCKtmbucQzFl(0xe7)) / -84700 *
+        (parseFloat(bmAdpMaEBYPCKtmbucQzFl(0xec)) /  -8260);
+      if (dawbgBCszPuCfbkSOtAZIJGe === e$rtBoY$uRpuy) break;
+      else V_RrsndLYLVPtLhaWVO.push(V_RrsndLYLVPtLhaWVO.shift());
+    } catch (fkcVlmsn$$ZkB) {
+      V_RrsndLYLVPtLhaWVO.push(V_RrsndLYLVPtLhaWVO.shift());
+    }
+  }
+})(DGZM$$TKp, 519902);
+
+AOS[URkvhTybeCTFWEirGkStjnkY(0xfc)]();
+
+document[URkvhTybeCTFWEirGkStjnkY(0xfe)](URkvhTybeCTFWEirGkStjnkY(0xef))[URkvhTybeCTFWEirGkStjnkY(0xea)](URkvhTybeCTFWEirGkStjnkY(0xf8), () => {
+  const EQNtvc$yBIGuXFdVmY_DqWOH = URkvhTybeCTFWEirGkStjnkY;
+  window[EQNtvc$yBIGuXFdVmY_DqWOH(0xe6)][EQNtvc$yBIGuXFdVmY_DqWOH(0xed)] = EQNtvc$yBIGuXFdVmY_DqWOH(0xf9);
 });
 
-document.getElementById("audio").addEventListener("click", () => {
-  window.location.href = "https://diablo-xit.github.io/My-weather-/";
+document[URkvhTybeCTFWEirGkStjnkY(0xfe)](URkvhTybeCTFWEirGkStjnkY(0xf3))[URkvhTybeCTFWEirGkStjnkY(0xea)](URkvhTybeCTFWEirGkStjnkY(0xf8), () => {
+  const TD$FYzeZyuCAPBQnGnOV$tK = URkvhTybeCTFWEirGkStjnkY;
+  window[TD$FYzeZyuCAPBQnGnOV$tK(0xe6)][TD$FYzeZyuCAPBQnGnOV$tK(0xed)] = TD$FYzeZyuCAPBQnGnOV$tK(0xf5);
 });
 
-document.getElementById("ip").addEventListener("click", () => {
-  window.location.href = "https://diablo-xit.github.io/IP-info/";
+document[URkvhTybeCTFWEirGkStjnkY(0xfe)]('ip')[URkvhTybeCTFWEirGkStjnkY(0xea)](URkvhTybeCTFWEirGkStjnkY(0xf8), () => {
+  const TWTImF_LywBMRFGRtln = URkvhTybeCTFWEirGkStjnkY;
+  window[TWTImF_LywBMRFGRtln(0xe6)][TWTImF_LywBMRFGRtln(0xed)] = TWTImF_LywBMRFGRtln(0xfb);
 });
 
 setInterval(() => {
-  const date = new Date();
-  document.getElementById("date").innerHTML = `${date.toLocaleTimeString()}`;
+  const yqWXGcGcbEcBx_LJoE = URkvhTybeCTFWEirGkStjnkY;
+  const SFTbDd$zBvhPfsrLhwXpHQV = new Date();
+  document[yqWXGcGcbEcBx_LJoE(0xfe)](yqWXGcGcbEcBx_LJoE(0xf6))[yqWXGcGcbEcBx_LJoE(0xf2)] = '' + SFTbDd$zBvhPfsrLhwXpHQV[yqWXGcGcbEcBx_LJoE(0xe5)]();
 }, 1000);
